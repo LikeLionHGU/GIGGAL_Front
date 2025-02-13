25be8cfabe0395e0969a25055ad5f1ad97d7bb1e
yoonchae <22400147@handong.ac.kr>
2025-02-13 16:54:05 +0900

    feat: footer 완성

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,26 @@
-
+import React from 'react';
+import Bookgie from "../../img/Bookgie.png";
+import SNS from "../../img/sns.png";
 import './Footer.css';
 
-const Footer = () => {
-  
-};
 
-export default Footer;
+const Footer = () => {
+    return (
+   
+      <div className="footer">
+        <div className="container">
+        <img src={Bookgie} alt="부기 로고" style={{ height: "50px" }} />
+        <div className="footert">책 찾기부터 몰입까지 이어주는 나의 책읽기 도우미<br/>BOOGIE ON AND ON !</div>
+        </div>
+        <div className="copy">
+        Copyright © 2025 Ease Crop. Created by Bookgie
+        </div>
+        <img src={SNS} alt="부기 로고" className='sns'/>
+
+      </div>
+   
+
+    );
+  };
+  
+  export default Footer;
