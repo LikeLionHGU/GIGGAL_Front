df1621af590ec8c144fa6af0731d8d0214bd88ab
JinHeeWond <jin2749708@handong.ac.kr>
2025-02-15 13:01:09 +0900

    feat: 컨테이너 변경

--- src/Pages/SearchDetail.js
+++ src/Pages/SearchDetail.js
@@ -1,11 +1,16 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import styles from "../styles/searchdetail.module.css";
 import HomeHeader from '../components/header/SearchHeader.js';
+import bookmarkIcon from "../img/bookmark.png"; 
 
 const SearchDetail = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const book = location.state?.book; // Search.js에서 전달된 책 정보
+  const book = location.state?.book;
+
+   // 로컬스토리지에서 북마크 개수 가져오기
+   const storedBookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+   const bookmarkCount = storedBookmarks.length;
 
   if (!book) {
     return <p className={styles.loading}>📚 책 정보를 찾을 수 없습니다.</p>;
@@ -17,7 +22,7 @@ const SearchDetail = () => {
       <div className={styles.container}>
         {/* 뒤로 가기 버튼 */}
         <button className={styles.backButton} onClick={() => navigate(-1)}>
-          🔙 뒤로가기
+          ←
         </button>
 
         {/* 책 정보 카드 */}
@@ -31,35 +36,43 @@ const SearchDetail = () => {
           )}
           <div className={styles.bookInfo}>
             <h1 className={styles.bookTitle}>{book.volumeInfo.title}</h1>
+             {/* 북마크 컨테이너 */}
+        
+        <div className={styles.bookmarkContainer}>
+              <span className={styles.bookmarkText}>
+                <img src={bookmarkIcon} alt="북마크" className={styles.bookmarkIcon} />
+                {bookmarkCount}
+              </span>
+              <span className={styles.readableText}>📖 읽을만해요</span>
+            </div>
+            <div className={styles.contain}>
             <p className={styles.bookAuthor}>
-              저자: {book.volumeInfo.authors?.join(", ") || "정보 없음"}
+              글 [{book.volumeInfo.authors?.join(", ") || "정보 없음"}]
             </p>
             <p className={styles.bookPublisher}>
-              출판사: {book.volumeInfo.publisher || "정보 없음"}
+               {book.volumeInfo.publisher || "정보 없음"}
             </p>
             <p className={styles.bookPages}>
               쪽수: {book.volumeInfo.pageCount || "정보 없음"}
             </p>
+            </div>
           </div>
+        </div>
+
+        {/* 탭 UI */}
+        <div className={styles.tabs}>
+          <div className={`${styles.tab} ${styles.active}`}>책 정보</div>
+          <div className={styles.tab}>커뮤니티</div>
         </div>
 
         {/* 책 설명 */}
         <div className={styles.bookContent}>
-          <h2>책 소개</h2>
           <p className={styles.bookDescription}>
             {book.volumeInfo.description || "설명 없음"}
           </p>
         </div>
 
-        {/* 버튼 영역 */}
-        <a
-          href={book.volumeInfo.infoLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.button}
-        >
-          📖 구글 도서에서 보기
-        </a>
+       
       </div>
     </div>
   );
